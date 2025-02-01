a1011d1fd9d2340e72d509332792b2294b520744
Alejandrette <alejandrette5117@gmail.com>
2025-02-01 17:43:13 +0000

    fix create group

--- src/front/js/pages/Groups.jsx
+++ src/front/js/pages/Groups.jsx
@@ -37,6 +37,9 @@ export function Groups() {
   }, [user, group]);
 
   useEffect(() => {
+    if (!group || !user) return;
+
+    // Obtener usuarios del grupo
     const getUserGroup = async () => {
       try {
         const response = await fetch(`${process.env.BACKEND_URL || 'http://localhost:3001/'}api/get_users_group/${group?.id}`)
@@ -49,23 +52,50 @@ export function Groups() {
       }
     }
 
+    // Obtener finanzas
+    const fetchFinances = async () => {
+      try {
+        const response = await fetch(`${process.env.BACKEND_URL || 'http://localhost:3001/'}api/get_finances_group/${group?.id}`);
+        const data = await response.json();
+        console.log("Ver finanzas del grupo", data); // Verifica los datos recibidos del backend
+
+        if (response.status === 200) {
+          setFinances(data);
+        }
+      } catch (error) {
+        console.error('Error al obtener las finanzas:', error);
+      }
+    };
+
+    // Obtener finanzas
+    const getFinancesUsers = async () => {
+      try {
+        const response = await fetch(`${process.env.BACKEND_URL || 'http://localhost:3001/'}api/get_finances/${user.id}`);
+        const data = await response.json();
+
+        if (response.status === 200) {
+          setFinanceUser(data);
+        }
+      } catch (error) {
+        console.error('Error al obtener las finanzas:', error);
+      }
+    };
+
     getUserGroup()
     fetchFinances();
     getFinancesUsers();
-  }, [group])
+  }, [group, user])
 
   //Obetener el Grupo del usuario
   const getGroup = async () => {
     try {
       const response = await fetch(`${process.env.BACKEND_URL || 'http://localhost:3001/'}api/get_user_group/${user.id}`)
+      const data = await response.json()
 
-      const updatedGroup = await response.json()
       if (response.status === 200) {
-        setGroup(updatedGroup);
-        localStorage.setItem('group', JSON.stringify(updatedGroup));
-      }
-
-      if (response.status !== 200) {
+        setGroup(data);
+        localStorage.setItem('group', JSON.stringify(data));
+      } else {
         console.log(data)
       }
     } catch (error) {
@@ -81,17 +111,15 @@ export function Groups() {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ 'id_group': id_group }),
       })
-
       const data = await response.json()
+
       if (response.status === 200) {
         setUser(prevUser => ({
           ...prevUser,
           id_group: id_group
         }))
         await changeRol({ id_rol: 1 })
-      }
-
-      if (response.status !== 200) {
+      } else {
         console.log(data)
       }
     } catch (error) {
@@ -105,24 +133,22 @@ export function Groups() {
       const response = await fetch(`${process.env.BACKEND_URL || 'http://localhost:3001/'}api/create_groups`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ 'name': name, 'description': description })
+        body: JSON.stringify({ name, description })
       })
-
       const data = await response.json()
+
       if (response.status === 200) {
         localStorage.setItem('group', JSON.stringify(data))
         setGroup(data)
         await addUserToGroup({ id_group: data.id })
-        location.reload();
-      }
-
-      if (response.status !== 200) {
+        location.reload()
+      } else {
         console.log('Error al crear grupo', response)
       }
     } catch (error) {
       console.log('Error al crear grupo', error)
     }
-  }
+  };
 
   // Cambiar el rol del usuario que crea el grupo a administrador
   const changeRol = async ({ id_rol }) => {
@@ -132,8 +158,8 @@ export function Groups() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ "id_rol": id_rol }) // 1 es el id del rol de administrador y 2 el invitado
       })
-
       const data = await response.json()
+
       if (response.status === 200) {
         setUser(prevUser => ({
           ...prevUser,
@@ -143,9 +169,7 @@ export function Groups() {
           ...user,
           id_rol: id_rol
         }));
-      }
-
-      if (response.status !== 200) {
+      } else {
         console.log(data)
       }
     } catch (error) {
@@ -161,16 +185,14 @@ export function Groups() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ "id_group": group.id }),
       })
-
       const data = await response.json()
+
       if (response.status === 200) {
         localStorage.removeItem('group')
         setGroup(null)
         await changeRol({ id_rol: 2 })
-        setInterval(() => { location.reload() }, 1000)
-      }
-
-      if (response.status !== 200) {
+        //setInterval(() => { location.reload() }, 1000)
+      } else {
         console.log(data)
       }
     } catch (error) {
@@ -186,19 +208,16 @@ export function Groups() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ "name": nameGroup })
       })
-
       const data = await response.json()
+
       if (response.status === 200) {
-        console.log(data)
         setGroup(prevGroup => ({
           ...prevGroup,
           name: nameGroup
         }))
-        location.reload()
+        // location.reload()
         await getGroup()
-      }
-
-      if (response.status !== 200) {
+      } else {
         console.log(data)
       }
     } catch (error) {
@@ -214,13 +233,11 @@ export function Groups() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ 'email': email, 'id_group': group.id })
       })
-
       const data = await response.json()
+
       if (response.status === 200) {
         console.log(data)
-      }
-
-      if (response.status !== 200) {
+      } else {
         console.log(data)
       }
     } catch (error) {
@@ -228,37 +245,8 @@ export function Groups() {
     }
   }
 
-  // Obtener finanzas
-  const fetchFinances = async () => {
-    try {
-      const response = await fetch(`${process.env.BACKEND_URL || 'http://localhost:3001/'}api/get_finances_group/${group?.id}`);
-      const data = await response.json();
-      console.log(data); // Verifica los datos recibidos del backend
-
-      if (response.status === 200) {
-        setFinances(data);
-      }
-    } catch (error) {
-      console.error('Error al obtener las finanzas:', error);
-    }
-  };
-
-  // Obtener finanzas
-  const getFinancesUsers = async () => {
-    try {
-      const response = await fetch(`${process.env.BACKEND_URL || 'http://localhost:3001/'}api/get_finances/${user.id}`);
-      const data = await response.json();
-
-      if (response.status === 200) {
-        setFinanceUser(data);
-      }
-    } catch (error) {
-      console.error('Error al obtener las finanzas:', error);
-    }
-  };
-
   // Añadir finanza al grupo
-  const addGroupFinance = async () => {
+  /* const addGroupFinance = async () => {
     const response = await fetch(`${process.env.BACKEND_URL || 'http://localhost:3001/'}api/add_group_finance`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -269,7 +257,6 @@ export function Groups() {
         "date": new Date().toISOString().split("T")[0],
       }),
     });
-
     const responseData = await response.json();
 
     if (response.ok) {
@@ -289,7 +276,7 @@ export function Groups() {
     if (selectedFinance) {
       addGroupFinance();
     }
-  };
+  }; */
 
   // Manejar el evento de submit del formulario de creación de grupo y llamar a la función de creación de grupo
   const handleSubmit = (e) => {
@@ -436,7 +423,7 @@ export function Groups() {
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
-              <form onSubmit={handleAddFinanceSubmit}>
+              <form /* onSubmit={handleAddFinanceSubmit} */>
                 <div className="mb-3">
                   <label htmlFor="selectFinance" className="form-label">Seleccionar Finanza</label>
                   <select
@@ -461,7 +448,7 @@ export function Groups() {
                   <button
                     type="submit"
                     className="btn btn-primary"
-                    onClick={addGroupFinance}
+                    /* onClick={addGroupFinance} */
                     disabled={financeAdded} // Deshabilitar el botón si ya se añadió una finanza
                   >
                     Añadir Finanza
